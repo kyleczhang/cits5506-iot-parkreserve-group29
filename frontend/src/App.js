ab0cd9a6d617ed3517b66d3a984c7967f8fac8f6
Riya Sakhiya <rheasakhiya0309@gmail.com>
2026-04-18 23:41:26 +0800

    Frontend initial setup with basic parking grid UI

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ParkingGrid from './components/ParkingGrid';
+
+function App() {
+  return (
+    <div>
+      <h1>ParkReserve Dashboard</h1>
+      <ParkingGrid />
+    </div>
+  );
+}
+
+export default App;
